07396cd3cb483891ba4abb09822fe6e343d09328
Justin Kenney <justinkenney13@gmail.com>
2018-11-09 17:22:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,9 +15,9 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     # binding.pry
-    params[:number].to_i.times do
-      puts "#{params[:phrase]}"
-    end
+    <%params[:number].to_i.times do%>
+      <%="#{params[:phrase]}"%>
+    <%end%>
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
