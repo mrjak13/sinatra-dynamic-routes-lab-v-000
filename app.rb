62a79f9c76d56add81f84d00c1740752855b8fe8
Justin Kenney <justinkenney13@gmail.com>
2018-11-09 18:32:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
-    binding.pry
+    # binding.pry
     if params[:operation] == "add"
       @value = number1+number2
       # "#{value}"
