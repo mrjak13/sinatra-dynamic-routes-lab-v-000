55c1059e99ba66582d6d18addf2d8b69aef8267d
Justin Kenney <justinkenney13@gmail.com>
2018-11-09 17:08:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -27,19 +27,19 @@ class App < Sinatra::Base
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
     if params[:operation] == "add"
-      value = number1 + number2
+      value = number1+number2
       "#{value}"
     elsif params[:operation] == "subtract"
-      value = number1 - number 2
+      value = number1-number 2
       "#{value}"
     elsif params[:operation == "multiply"
-      value = number1 * number 2
+      value = number1*number 2
       "#{value}"
     elsif params[:operation] == "divide"
-      value = number1 / number2
+      value = number1/number2
       "#{value}"
     else
-      "I didn't understand that"            
+      "I didn't understand that"
   end
 
 end
