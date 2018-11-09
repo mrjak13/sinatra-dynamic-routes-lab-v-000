d9aa7afcdfebab093e8540e6240b22888163ddc1
Justin Kenney <justinkenney13@gmail.com>
2018-11-09 18:40:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,10 +14,12 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
+    @array = []
     number = params[:number].to_i
     phrase = params[:phrase]
     result = number.times{"#{phrase}"}
-    params[:number].to_i.times{puts "#{params[:phrase]}"}
+    params[:number].to_i.times{@array << "#{params[:phrase]}"}
+    "#{@array}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
